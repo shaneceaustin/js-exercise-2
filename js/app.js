4e6a418d7758c5cf47c21c30b6fcdacd430edb33
NicoleIrene <ace_e303@e303-s13.lab.lagcc>
2017-06-09 16:45:37 -0400

    added closed functionality

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
+var toDoList = document.querySelector('#to-do-list')
 var userSubmit = document.querySelector('#user-submit') 
 
 function addToDo(event) {
 	event.preventDefault();
 	
 	var userInput = document.querySelector ('#user-input')
-	var toDoList = document.querySelector('#to-do-list')
+	
 
 	if(userInput.value === ''){
 		return false
 	}
-toDoList.innerHTML = '<li>' + userInput.value + '</li>' + toDoList.innerHTML;
+
+toDoList.innerHTML = '<li><i class="fa fa-square-o close-to-do" aria-hidden="true"></i> ' + userInput.value + '</li>' + toDoList.innerHTML;
 
 userInput.value = '';
 
 }
+function removeToDo(event) {
+	;
+	if(event.target.classList.contains('close-to-do')){
+	  var li = event.target.parentElement;
+	  toDoList.removeChild(li);
+	}
+}
+
+toDoList.addEventListener('click', removeToDo, false);
+
 userSubmit.addEventListener('click', addToDo, false);
+
+
+
+// <i class="fa fa-thumbs-up" aria-hidden="true">
